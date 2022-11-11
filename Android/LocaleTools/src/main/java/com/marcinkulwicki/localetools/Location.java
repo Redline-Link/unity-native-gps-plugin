09114a92e0c39df3754c3628d43612b6156172ed
Link <68460104+Redline-Link@users.noreply.github.com>
2022-11-11 10:38:41 +0800

    fix crash when enable service on API level 29 phone

--- Android/LocaleTools/src/main/java/com/marcinkulwicki/localetools/Location.java
+++ Android/LocaleTools/src/main/java/com/marcinkulwicki/localetools/Location.java
@@ -23,4 +23,30 @@ public class Location implements LocationListener
     {
         LastLocation = location;
     }
+
+    // FIXME hasn't test the new one with different params
+//    @Override
+//    public void onLocationChanged(List<android.location.Location> locations) {
+//        //LocationListener.super.onLocationChanged(locations);
+//        if (locations.size() > 1) {
+//            Log.d("NativeGPS", "location count: " + locations.size());
+//            LastLocation = locations.get(locations.size()-1);
+//        }
+//    }
+
+    // need to implement those callback or it'll just crash on API level 29 phone
+    @Override
+    public void onFlushComplete(int requestCode) {
+        //LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //LocationListener.super.onProviderDisabled(provider);
+    }
 }
